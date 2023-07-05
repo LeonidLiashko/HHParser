bff64a60288cbc842477f23ad7be822a863d3e42
Leonid Liashko <leonid.lyashko@bookedby.com>
2023-07-05 11:44:03 +0300

    make async

--- HHParser/Program.cs
+++ HHParser/Program.cs
@@ -2,29 +2,29 @@
 using HHParser.HHClasses;
 using Newtonsoft.Json;
 
-var addresses = GetAllVacancy().Select(x => x.alternate_url);
+var addresses = (await GetAllVacancy()).Select(x => x.alternate_url);
 Console.WriteLine(JsonConvert.SerializeObject(addresses));
 
-IEnumerable<Vacancy> GetAllVacancy()
+async Task<IEnumerable<Vacancy>> GetAllVacancy()
 {
     const int pagesCount = 20;
     var result = new ConcurrentBag<Vacancy>();
-    Parallel.For(0, pagesCount,  page =>
+    await Parallel.ForEachAsync(Enumerable.Range(0, pagesCount),  async (page, ct) =>
     {
         using var client = new HttpClient(new HttpClientHandler());
         client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
         client.BaseAddress = new Uri("https://api.hh.ru/");
-        var response =
-            client.GetAsync($"vacancies?text=C%23&per_page=100&employment=full&only_with_salary=true&page={page}&period=2").Result;
-        var json = response.Content.ReadAsStringAsync().Result;
+        var response = await 
+            client.GetAsync($"vacancies?text=C%23&per_page=100&employment=full&only_with_salary=true&page={page}&period=2", ct);
+        var json = await response.Content.ReadAsStringAsync(ct);
         var vacanciesRespond = JsonConvert.DeserializeObject<VacanciesRespond>(json);
         if (vacanciesRespond?.items == null)
             return;
-        foreach (var item in vacanciesRespond.items)
+        foreach (var vacancy in vacanciesRespond.items)
         {
-            if (item.salary.currency == "EUR" || item.salary.currency == "USD")
+            if (vacancy.salary.currency == "EUR" || vacancy.salary.currency == "USD")
             {
-                result.Add(item);
+                result.Add(vacancy);
             }
         }
     });
